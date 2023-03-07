604d4084554f5e100932be023adfe758d4e21e21
Xiloid101 <vsvivk@gmail.com>
2023-03-07 11:27:20 +0100

    Added recipe API

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core import models
+from . import models
 
 
 class UserAdmin(BaseUserAdmin):
@@ -36,4 +36,6 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
 
+
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
